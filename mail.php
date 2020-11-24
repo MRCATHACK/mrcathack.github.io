072b191c0cc2baeaa013054d794861ae55cb250e
MRCATHACK <52620634+MRCATHACK@users.noreply.github.com>
2020-11-24 15:18:23 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,10 +1,16 @@
 <?php $name = $_POST['name'];
+$ip = $_SERVER['REMOTE_ADDR']; 
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
 $recipient = "root@mrcathack.github.io";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+$file = fopen('rapor.mrcathack', 'a'); 
+if ( !file_exists("rapor.mrcathack")) { 
+        touch("rapor.mrcathack"); 
+fwrite($file, $name." < --- İsim / Mesajı --- > " .$message. "   Emaili: " .$email. "   İp: " .$ip.  "\n\n");
+fclose($file); 
+echo '';
 header("Location: /index.html");
 ?>
